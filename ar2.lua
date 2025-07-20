67f238ca6ce526414a8123eb4b57298f5592db57
PrismScope <44588804+Klinac@users.noreply.github.com>
2025-07-20 20:49:45 +0200

    Update ar2.lua

--- ar2.lua
+++ ar2.lua
@@ -14,7 +14,7 @@ local UI_Theme = 'Dark' -- Can be 'Dark', 'Light', 'Nord', 'Solarized' or 'Dracu
 -- Ad-link settings
 local Linkvertise_Enabled = true
 local Linkvertise_Link =
-    'https://ads.luarmor.net/get_key?for=AR2_Reward-IgvcvJYXZHBx'
+    'https://ads.luarmor.net/get_key?for=AR2_KEY-IgvcvJYXZHBx'
 
 local Lootlabs_Enabled = false
 local Lootlabs_Link = ''
